efe6f6b398fc96d240cb9cd7b8b4c4eaff399754
sevgit <sevg1993@gmail.com>
2020-10-12 20:37:28 -0300

    Draw foreign cursor

--- assets/js/wye_canvas.js
+++ assets/js/wye_canvas.js
@@ -3,11 +3,12 @@ class WyeCanvas {
         this.socket = socket;
         this.channel = this.socket.channel('wye_canvas:lobby', {});
         this.channel.join();
-        this.setupCanvas();
+        this.setupCanvases();
         this.setupListeners();
+        this.id = Math.random() * 100
     }
 
-    setupCanvas = () => {
+    setupCanvases = () => {
         this.canvas = document.createElement('canvas');
         document.body.appendChild(this.canvas);
         document.body.style.margin = 0;
@@ -15,36 +16,48 @@ class WyeCanvas {
         this.ctx = this.canvas.getContext('2d');
         this.ctx.canvas.width = window.innerWidth;
         this.ctx.canvas.height = window.innerHeight;
-        /*         this.foreignCursor = new Image();
-                this.foreignCursor.src = "/images/cursor.svg"
-                this.foreignCursor.width = 10;
-                this.foreignCursor.height = 10; */
+
+        // Cursor canvas
+        this.cursorCanvas = document.createElement('canvas');
+        document.body.appendChild(this.cursorCanvas);
+        document.body.style.margin = 0;
+        this.cursorCanvas.style.position = 'fixed';
+        this.cursorCtx = this.cursorCanvas.getContext('2d');
+        this.cursorCtx.canvas.width = window.innerWidth;
+        this.cursorCtx.canvas.height = window.innerHeight;
+
+
+        this.foreignCursor = new Image();
+        this.foreignCursor.src = "/images/cursor.svg"
+        this.foreignCursor.width = 10;
+        this.foreignCursor.height = 10;
     }
 
     setupListeners = () => {
         document.addEventListener('mousemove', this.shoutDrawing);
         document.addEventListener('touchmove', this.shoutDrawing);
         this.channel.on('shout', payload => {
-            console.log(payload)
             this.draw(payload)
         })
     }
 
-    draw = ({ clientX, clientY }) => {
-        /*      this.ctx.drawImage(this.foreignCursor, clientX, clientY); */
-        this.ctx.beginPath(); // begin
-        this.ctx.moveTo(clientX, clientY); // from
+    draw = ({ clientX, clientY, isDrawing, id }) => {
+        if (id !== this.id) {
+            this.cursorCtx.clearRect(0, 0, this.cursorCanvas.width, this.cursorCanvas.height);
+            this.cursorCtx.drawImage(this.foreignCursor, clientX, clientY);
+        }
+        if (!isDrawing) return;
+        this.ctx.beginPath();
+        this.ctx.moveTo(clientX, clientY);
         this.ctx.lineWidth = 5;
         this.ctx.lineCap = 'round';
         this.ctx.strokeStyle = '#c0392b';
-        this.ctx.lineTo(clientX, clientY); // to
-        this.ctx.stroke(); // draw it!
+        this.ctx.lineTo(clientX, clientY);
+        this.ctx.stroke();
     }
 
     shoutDrawing = ({ clientX, clientY, buttons }) => {
-        if (buttons !== 1) return;
-        console.log({ clientX, clientY })
-        this.channel.push('shout', { clientX, clientY })
+        this.channel.push('shout', { clientX, clientY, isDrawing: buttons === 1, id: this.id })
     }
 
 }
